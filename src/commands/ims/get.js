d289bc98a4dee1145f20dd602e99484c22124385
Felix Meschberger <fmeschbe@adobe.com>
2019-07-05 14:03:24 +0200

    Remove unused "flags"

--- src/commands/ims/get.js
+++ src/commands/ims/get.js
@@ -10,7 +10,6 @@ OF ANY KIND, either express or implied. See the License for the specific languag
 governing permissions and limitations under the License.
 */
 
-const { flags } = require('@oclif/command')
 const ImsCallCommand = require('../../ims-call-command')
 const debug = require('debug')('@adobe/aio-cli-plugin-ims/get');
 
